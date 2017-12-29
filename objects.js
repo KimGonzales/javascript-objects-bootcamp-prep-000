abd12651370fc10d965459700507753a583d1fb8
Kim Gonzales <kimgo589@gmail.com>
2017-12-29 22:08:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,9 @@
 var meals = {};
 var meals = new Object ();
 
+var playlist = { artistNames : "songTitles" }
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
